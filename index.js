b7ed2be64d02322147596d515386732726d2c01b
EnderHack93 <75341553+EnderHack93@users.noreply.github.com>
2023-10-30 23:29:53 -0400

    error force true

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { sequelize } from "./src/config/sequelize.js";
 
 try {
   try {
-    await sequelize.sync({force:true}); 
+    await sequelize.sync({force:false}); 
     console.log("Conexion exitosa a la base de datos");
   } catch (error) {
     console.log(error.message);
